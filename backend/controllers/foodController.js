657315baf860e63e071872ebb720d716713296f5
yeshwanth <yeshwanthyadavkondra@gmail.com>
2025-03-16 13:48:38 +0530

    Crud apis of food

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -5,10 +5,10 @@ const addFood = async (req, res) => {
   let image_filename = `${req.file.filename}`;
   const food = new foodModel({
     name: req.body.name,
-    image: image_filename,
-    price: req.body.price,
     description: req.body.description,
+    price: req.body.price,
     category: req.body.category,
+    image: image_filename,
   });
   try {
     await food.save();
@@ -22,4 +22,31 @@ const addFood = async (req, res) => {
   }
 };
 
-export { addFood };
+// all food list
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    return res.status(200).json({ success: true, data: foods });
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ success: false, message: err.message || "Something went wrong" });
+  }
+};
+
+// remove food item
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, () => {});
+    await foodModel.findByIdAndDelete(req.body.id);
+    return res
+      .status(200)
+      .json({ success: true, message: "Food removed successfully" });
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ success: false, message: err.message || "Something went wrong" });
+  }
+};
+export { addFood, listFood, removeFood };
